9feaa742f9e30ceb70135e5e1742ff60029a928e
minjunseok6565-pixel <minjunseok6565@gmail.com>
2026-02-11 18:26:57 +0900

    Delete generation 3번 directory

--- generation 3번/dealgen/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-"""Internal implementation package for the deal generator.
-
-This package is a pure refactor-split of the original monolithic deal generator module.
-"""
