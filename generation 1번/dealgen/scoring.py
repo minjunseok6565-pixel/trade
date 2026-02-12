88aa8dc87d3d7fe4a7e054659df0cb47f75880fa
minjunseok6565-pixel <minjunseok6565@gmail.com>
2026-02-12 14:27:33 +0900

    Update scoring.py

--- generation 1번/dealgen/scoring.py
+++ generation 1번/dealgen/scoring.py
@@ -1,51 +1,14 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from datetime import date
-from enum import Enum
-import hashlib
-import json
 import math
-import random
-from typing import Any, Dict, Iterable, List, Mapping, Optional, Sequence, Set, Tuple
+from typing import List, Optional, Tuple
 
-from ...errors import (
-    TradeError,
-    DEAL_INVALIDATED,
-    ROSTER_LIMIT,
-    ASSET_LOCKED,
-    PLAYER_NOT_OWNED,
-    PICK_NOT_OWNED,
-    SWAP_NOT_OWNED,
-    TRADE_DEADLINE_PASSED,
-    DUPLICATE_ASSET,
-)
-from ...models import (
-    Deal,
-    PlayerAsset,
-    PickAsset,
-    SwapAsset,
-    Asset,
-    asset_key,
-    canonicalize_deal,
-    serialize_deal,
-    resolve_asset_receiver,
-)
+from ...errors import TradeError
+from ...models import Deal, PlayerAsset, PickAsset, SwapAsset
 from ...valuation.service import evaluate_deal_for_team
 from ...valuation.types import DealDecision, DealVerdict, TeamDealEvaluation
 
 from ..generation_tick import TradeGenerationTickContext
-from ..asset_catalog import (
-    TradeAssetCatalog,
-    IncomingPlayerRef,
-    TeamOutgoingCatalog,
-    PlayerTradeCandidate,
-    PickTradeCandidate,
-    SwapTradeCandidate,
-    PickBucketId,
-    BucketId,
-    build_trade_asset_catalog,
-)
 
 from .types import DealGeneratorConfig, DealProposal, DealGeneratorStats
 
