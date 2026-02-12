9f85ff4ffa94ded80b36cf70778142845298e9d0
minjunseok6565-pixel <minjunseok6565@gmail.com>
2026-02-12 14:30:03 +0900

    Update targets.py

--- generation 1번/dealgen/targets.py
+++ generation 1번/dealgen/targets.py
@@ -1,51 +1,11 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
 from datetime import date
-from enum import Enum
-import hashlib
-import json
-import math
 import random
-from typing import Any, Dict, Iterable, List, Mapping, Optional, Sequence, Set, Tuple
-
-from ...errors import (
-    TradeError,
-    DEAL_INVALIDATED,
-    ROSTER_LIMIT,
-    ASSET_LOCKED,
-    PLAYER_NOT_OWNED,
-    PICK_NOT_OWNED,
-    SWAP_NOT_OWNED,
-    TRADE_DEADLINE_PASSED,
-    DUPLICATE_ASSET,
-)
-from ...models import (
-    Deal,
-    PlayerAsset,
-    PickAsset,
-    SwapAsset,
-    Asset,
-    asset_key,
-    canonicalize_deal,
-    serialize_deal,
-    resolve_asset_receiver,
-)
-from ...valuation.service import evaluate_deal_for_team
-from ...valuation.types import DealDecision, DealVerdict, TeamDealEvaluation
+from typing import Any, Dict, List, Optional, Sequence, Set, Tuple
 
 from ..generation_tick import TradeGenerationTickContext
-from ..asset_catalog import (
-    TradeAssetCatalog,
-    IncomingPlayerRef,
-    TeamOutgoingCatalog,
-    PlayerTradeCandidate,
-    PickTradeCandidate,
-    SwapTradeCandidate,
-    PickBucketId,
-    BucketId,
-    build_trade_asset_catalog,
-)
+from ..asset_catalog import TradeAssetCatalog, IncomingPlayerRef, TeamOutgoingCatalog
 
 from .types import DealGeneratorConfig, DealGeneratorBudget, TargetCandidate, SellAssetCandidate
 
